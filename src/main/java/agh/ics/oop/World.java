bd0092611d04c3707612d1e4af4305ec0c9f0f05
Blazej Nowicki <blazej.a.now@gmail.com>
2021-10-14 21:55:54 +0200

    After task 18

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,29 +3,51 @@ package agh.ics.oop;
 import static java.lang.System.out;
 
 public class World {
-
-    private static void run(String[] directions){
+    private static void run(Direction[] directions){
         out.println("Start");
-        for (String s: directions) {
+        for (Direction s: directions) {
             switch (s){
-                case "f":
+                case FORWARD:
                     out.println("Zwierzak idzie do przodu");
                     break;
-                case "b":
+                case BACKWARD:
                     out.println("Zwierzak idzie do tyłu");
                     break;
-                case "r":
+                case RIGHT:
                     out.println("Zwierzak skręca w prawo");
                     break;
-                case "l":
+                case LEFT:
                     out.println("Zwierzak skręca w lewo");
                     break;
             }
         }
         out.println("Stop");
     }
+
+    private static Direction[] translate(String[] dirs) {
+        Direction[] dirs_enum = new Direction[dirs.length];
+        for (int i=0; i< dirs.length; i++) {
+            switch (dirs[i]) {
+                case "f":
+                    dirs_enum[i] = Direction.FORWARD;
+                    break;
+                case "b":
+                    dirs_enum[i] = Direction.BACKWARD;
+                    break;
+                case "r":
+                    dirs_enum[i] = Direction.RIGHT;
+                    break;
+                case "l":
+                    dirs_enum[i] = Direction.LEFT;
+                    break;
+            }
+        }
+        return dirs_enum;
+    }
+
     public static void main(String[] args) {
         String[] dirs  = {"f", "f", "r", "l"};
-        run(dirs);
+        Direction[] dirs_translated = translate(dirs);
+        run(dirs_translated);
     }
 }
